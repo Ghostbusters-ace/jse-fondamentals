110f161ca2bac6942acc1d1e5f7b462616be8f49
lionj <judin.malivert@poe.aelion.biz>
2023-02-22 10:00:56 +0100

    change CourseRun

--- src/main/java/fr/aelion/run/CourseRun.java
+++ src/main/java/fr/aelion/run/CourseRun.java
@@ -1,9 +1,7 @@
 package fr.aelion.run;
 
-import fr.aelion.models.course.Document;
-import fr.aelion.models.course.Media;
-import fr.aelion.models.course.Slide;
-import fr.aelion.models.course.Video;
+import fr.aelion.builder.MediaBuilder;
+import fr.aelion.models.course.*;
 import fr.aelion.repositories.course.Course;
 import fr.aelion.services.courses.DisplayCourse;
 
@@ -23,17 +21,64 @@ public class CourseRun {
         System.out.println(displaycourse.displaBuilder());
     }
     private void makeCourse(){
-        Media video = new Video();
+        MediaBuilder videoBuilder = new MediaBuilder();
+        videoBuilder.setMediaType("video");
+        videoBuilder
+                .title("Create table")
+                .summary("Creer une table")
+                .duration(5.35F)
+                .author(new Author());
+
+        MediaBuilder slideBuilder = new MediaBuilder();
+        slideBuilder.setMediaType("slide");
+        slideBuilder
+                .title("Alter table")
+                .summary("modifier une table")
+                .duration(3.30F)
+                .author(new Author());
+
+        MediaBuilder docBuilder = new MediaBuilder();
+        docBuilder.setMediaType("document");
+        docBuilder
+                .title("Drop table")
+                .summary("Supprimer une table")
+                .duration(1.10F)
+                .author(new Author());
+
+        MediaBuilder badBuilder = new MediaBuilder();
+        badBuilder
+                .title("gang")
+                .duration(0.10F);
+
+        /*Media video = new Video();
         video.setTitle("Create table");
 
         Media slide = new Slide();
         slide.setTitle("Alter table");
 
         Media document = new Document();
-        document.setTitle("Drop tbale");
+        document.setTitle("Drop tbale");*/
 
-        this.course.addMedia(video);
-        this.course.addMedia(slide);
-        this.course.addMedia(document);
+        try {
+            this.course.addMedia(videoBuilder.build());
+        } catch (Exception e) {
+            System.out.println("Unable to add video media");
+        }
+        try {
+            this.course.addMedia(slideBuilder.build());
+        } catch (Exception e) {
+            System.out.println("Unable to add slide media");
+        }
+        try {
+            this.course.addMedia(docBuilder.build());
+        } catch (Exception e) {
+            System.out.println("Unable to add document media");
+        }
+
+        try {
+            this.course.addMedia(badBuilder.build());
+        } catch (Exception e) {
+            System.out.println("bad bad bad");
+        }
     }
 }
